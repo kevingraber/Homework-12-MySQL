2bb91aac8f4492fa5b5b26c93dbab710b99dc43a
kg <kjg310@gmail.com>
2016-04-18 13:57:08 -0400

    Prompt working, need to add/remove from database now

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require('mysql');
 var password = require('./password.js');
+var prompt = require('prompt');
 
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -9,9 +10,10 @@ var connection = mysql.createConnection({
 });
 
 connection.connect();
+prompt.start();
 
 console.log(" ");
-console.log("Welcome to Kein's Store!");
+console.log("Welcome to Kevin's Store!");
 console.log(" ");
 console.log("We have the following products for sale:");
 console.log(" ");
@@ -20,6 +22,25 @@ connection.query('SELECT * FROM products', function(err, rows) {
 	if (err) throw err;
 	for (var i = 0; i < rows.length; i++) {
 		console.log("Item ID: " + rows[i].ItemID + " Name: " + rows[i].ProductName + " Price: " + rows[i].Price);
+	};
+	var schema = {
+		properties: {
+			itemid: {
+				description: 'What is the ID of the product you want?'
+			},
+			quantity: {
+				description: 'How many would you like?'
+			}
+		}
 	}
-	// console.log(rows[0])
-})
+	prompt.get(schema, function(err, result) {
+		// console.log(rows[result.itemid]);
+		if (rows[result.itemid - 1].StockQuantity < result.quantity) {
+			console.log("Insufficient Quantity");
+		} else {
+			console.log("Yerp we can do that!")
+			console.log("Your order costs: $" + (rows[result.itemid-1].Price * result.quantity))
+			console.log("Thanks for shopping at Keivn's Store!")
+		};
+	});
+});
