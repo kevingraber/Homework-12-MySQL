c5454ecbeeac1f3c96884e0288eb839e467057ca
kg <kjg310@gmail.com>
2016-04-19 01:34:07 -0400

    Working on challenge 3

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -38,13 +38,21 @@ connection.query('SELECT * FROM products', function(err, rows) {
 		if (rows[result.itemid - 1].StockQuantity < result.quantity) {
 			console.log("Insufficient Quantity");
 		} else {
-			
+			var orderPrice = (rows[result.itemid-1].Price * result.quantity);
+			var department = rows[result.itemid-1].DepartmentName;
 			console.log("Yerp we can do that!");
-			console.log("Your order costs: $" + (rows[result.itemid-1].Price * result.quantity));
+			console.log("Your order costs: $" + orderPrice);
 			console.log("Thanks for shopping at Kevin's Store!");
 
+			connection.query('SELECT * FROM Departments', function(err, rows){
+				connection.query('UPDATE Departments SET TotalSales = TotalSales + ' + orderPrice + ' WHERE DepartmentName ="' + department + '"', function(err, res){
+					if (err) throw err;
+				});	
+			});
+			
+
 			var newQuantity = ( rows[result.itemid - 1].StockQuantity - result.quantity);
-			connection.query('UPDATE products SET StockQuantity=' + newQuantity + ' WHERE ItemID=' + result.itemid + ';', function(err, red) {
+			connection.query('UPDATE products SET StockQuantity=' + newQuantity + ' WHERE ItemID=' + result.itemid + ';', function(err, res) {
 				if (err) throw err;
 			});
 
